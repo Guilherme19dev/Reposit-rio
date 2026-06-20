083dffec21ed23223077f005a72a80a17954e9ae
Guilherme19dev <guilhermefacenda08@gmail.com>
2026-06-20 13:23:47 -0300

    versão inicial

---
+++ Calculadora/script.js
@@ -0,0 +1,90 @@
+numero=0;
+const botoes = document.querySelectorAll(".um");
+var btn1 = document.getElementById('one');
+var btn2 = document.getElementById('two');
+var btn3 = document.getElementById('three');
+var btn4 = document.getElementById('four');
+var btn5 = document.getElementById('five');
+var btn6 = document.getElementById('six');
+var btn7 = document.getElementById('seven');
+var btn8 = document.getElementById('eight');
+var btn9 = document.getElementById('nine');
+var btn0 = document.getElementById('zero');
+botoes.addEventListener("click",(event)=> {
+ if (true){
+    if(btn1){
+        document.getElementById('one').onClick=value=1;
+        alert('Button was clicked!:'+value);
+        numero=value;
+        alert(""+numero);
+        console.log("O numero é"+numero);
+     } else if (true){
+     } else if(btn2){
+            document.getElementById('two').onClick=value=2;
+            alert('Button was clicked!:'+value);
+            numero=value;
+            alert(""+numero);
+            console.log("O numero é"+numero);
+  } else if (true){
+   } else if(btn3){
+                document.getElementById('three').onClick=value=3;
+                alert('Button was clicked!:'+value);
+                numero=value;
+                alert(""+numero);
+                console.log("O numero é"+numero);
+                } else if (true){
+}else if(btn4) {
+                    document.getElementById('four').onClick=value=4;
+                    alert('Button was clicked!:'+value);
+                    numero=value;
+                    alert(""+numero);
+                    console.log("O numero é"+numero);
+                    } else if (true){
+ }else if(btn5){
+                        document.getElementById('five').onClick=value=5;
+                        alert('Button was clicked!:'+value);
+                        numero=value;
+                        alert(""+numero);
+                        console.log("O numero é"+numero);
+                        } else if (true){
+}else if(btn6) {
+                            document.getElementById('six').onClick=value=6;
+                            alert('Button was clicked!:'+value);
+                            numero=value;
+                            alert(""+numero);
+                            console.log("O numero é"+numero);
+                            } else if (true){
+}else if (btn7) {
+                                document.getElementById('seven').onClick=value=7;
+                                alert('Button was clicked!:'+value);
+                                numero=value;
+                                alert(""+numero);
+                                console.log("O numero é"+numero);
+                           } else if (true){
+                            }else if(btn8) {
+                                    document.getElementById('eight').onClick=value=8;
+                                    alert('Button was clicked!:'+value);
+                                    numero=value;
+                                    alert(""+numero);
+                                    console.log("O numero é"+numero);
+                                    } else if (true){
+}else if(btn9) {
+                                        document.getElementById('nine').onClick=value=9;
+                                        alert('Button was clicked!:'+value);
+                                        numero=value;
+                                        alert(""+numero);
+                                        console.log("O numero é"+numero);
+}else{
+                                                document.getElementById('zero').onClick=value=0;
+                                                alert('Button was clicked!:'+value);
+                                                numero=value;
+                                                alert(""+value);                                               
+                                                console.log("O numero é"+numero);
+ }
+//else{
+    //console.log("clique em um numero");
+//}
+//}
+ }})
+value=numero;
+console.log((""+value));
